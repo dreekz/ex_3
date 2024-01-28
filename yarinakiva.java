243cb57e973abdbdfcb3a734c169eb3e83809ce8
dreekz <yarin3db@gmail.com>
2024-01-28 17:50:52 +0200

    second edit commited

--- yarinakiva.java
+++ yarinakiva.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         for(int i=1; i<=5;i++){
-            System.out.println("Yarin Akiva" + i);
+            System.out.println("empty__" + i);
         }
     }
 }

63903b4b6a3f17c486cf3c6f090b04b0d4a4f429
dreekz <yarin3db@gmail.com>
2024-01-29 02:01:58 +0200

    Forth Commit

--- yarinakiva.java
+++ yarinakiva.java
@@ -1,7 +1,7 @@
 public class yarinakiva{
     public static void main(String[] args){
         for(int i=1; i<=5;i++){
-            System.out.println("empty__" + i);
+            System.out.println("This is the Change Of the world" + i);
         }
     }
 }
